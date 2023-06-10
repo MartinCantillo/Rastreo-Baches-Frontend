818d9f58ffe2d2bd4cf1838e8e3f3e2765a91285
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-06-10 08:30:45 -0500

    Creatting  service  of the baches

--- src/app/services/RegisterBaches.service.ts
+++ src/app/services/RegisterBaches.service.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { baches } from '../Models/Baches';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterBachesService {
 
-constructor() { }
+  private SaveBaches: string = 'http://localhost:8023/user/bache';
+  constructor(private httpClient: HttpClient) {}
+
+  public SaveUser(bache: baches): Observable<Object> {
+    return this.httpClient.post(`${this.SaveBaches}`, bache);
+  }
 
 }
